056b69542d79be7ae6522f4050f8627cc3c4480d
Alex Mello <alexmello@protonmail.com>
2020-04-26 20:32:40 -0300

    0.1

---
+++ find_num.py
@@ -0,0 +1,37 @@
+import re
+
+def find_num(num):
+    """Função para encontrar algorismos de 0-9 dentro de iteráveis
+    Retornará apenas aa primeira sequência de números encontrados
+    Esta função não procura por '.'(pontos) podendo alterar os valores
+    de floats ou números maiores que 999 (1.000)
+    Esta função retornará float dos valores encontrados ou 0(zero) caso
+    não encontre nenhum número."""
+    rex = re.compile(r'(\d+)').search(str(num))
+    if rex:
+        return float(rex.group(1))
+    else:
+        return 0
+
+def find_num_all(num):
+    """Função para encontrar algorismos de 0-9 dentro de iteráveis
+    Ela continua sua busca mesmo que encontre letras
+    Não procura por '.'(pontos), podendo alterar os valores de floats
+    ou números maiores que 999 (1.000)
+    Esta função retorna integers. Se não encontrar nenhum número, retornará 0."""
+    rex = re.compile(r'([0-9])')
+    if rex:
+        return int("".join(rex.findall(str(num))))
+    else:
+        return 0
+
+def find_num_(num):
+    """Função para encontrar números dentro de iteráveis
+    Ela também funciona para floats
+    Retornará uma string somente com números
+    Se não encontrar nenhum número, retornará 0"""
+    n = ''
+    for index, char  in enumerate(str(num)):
+        if char.isnumeric() or (char == '.' and num[index-1].isnumeric() and num[index+1].isnumeric()):
+            n += char
+    return n if n != "" else 0
